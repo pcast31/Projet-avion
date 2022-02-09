b356e522ca2d78d06913f1e783802579c4e7744c
Paul <paul.casteras@student-cs.fr>
2022-02-09 15:09:35 +0100

    classe individu

--- individu.py
+++ individu.py
@@ -2,11 +2,12 @@ dic_masse={'M':85,'F':70,'E':35}
 
 class Individu:
 
-    def __init__(self,i,cat):
+    def __init__(self,i,cat,t):
         self.id=i
         self.group=[]
         self.categorie=cat
         self.masse=dic_masse[cat]
+        self.transit=t
 
     def ajout_au_groupe(self,ind):
         self.group.append(ind)
