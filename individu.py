2ffe6163aec4f06af2da1b18eec9690c9ebb7cba
Robardet Alexis <alexis.robardet@student-cs.fr>
2022-02-23 14:54:58 +0100

    modifs pour affichage

--- individu.py
+++ individu.py
@@ -2,9 +2,10 @@ dic_masse={'H':85,'F':70,'E':35}
 
 class Individu:
 
-    def __init__(self,i,cat,t):
+    def __init__(self,i,cat,t, idgroupe):
         self.id=i
         self.categorie=cat
+        self.idgroupe = idgroupe
         self.groupe=[]
         self.masse=dic_masse[cat]
         self.transit=t
