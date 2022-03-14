477483c31f3a79d0da6e4646c58cffb7453da11e
Paul <paul.casteras@student-cs.fr>
2022-03-14 14:58:03 +0100

    petit groupe

--- lirexcel.py
+++ lirexcel.py
@@ -104,6 +104,53 @@ def lirexcel2(scenario, chemin='DataSeating.xlsx'):
     return l
 
 
+def reduction(scenario,ind, chemin='DataSeating.xlsx'):
+    df = pd.read_excel(chemin, sheet_name=scenario)
+    df = df.drop(df.tail(2).index)
+    df = df.fillna(0)
+    df['Numéro du groupe'] = df['Numéro du groupe'].apply(lambda x: int(x))
+    df['Femmes'] = df['Femmes'].apply(lambda x: int(x))
+    df['Hommes'] = df['Hommes'].apply(lambda x: int(x))
+    df['Enfants'] = df['Enfants'].apply(lambda x: int(x))
+    df['WCHR'] = df['WCHR'].apply(lambda x: int(x))
+    df['WCHB'] = df['WCHB'].apply(lambda x: int(x))
+    df['Classe'] = df['Classe'].apply(lambda x: 0 if x == 'Y' else 1)
+    df['TransitTime'] = df['TransitTime'].apply(lambda x: 0 if type(x) == int else 60 * x.hour + x.minute)
+
+    l = []
+    id = 0
+    taille_groupe={}
+    for individu in ind:
+        if individu.idgroupe not in taille_groupe:
+            taille_groupe[individu.idgroupe]=0
+            
+        taille=len(individu.groupe)+1
+        if taille<=3:
+            l.append(individu)
+
+        elif taille%3==1:
+            l.append(Individu(individu.id,individu.categorie,individu.transit,individu.idgroupe+200*((taille_groupe[individu.idgroupe]+1)//3),individu.classe))
+            taille_groupe[individu.idgroupe]+=1
+
+        else :
+            l.append(Individu(individu.id,individu.categorie,individu.transit,individu.idgroupe+200*(taille_groupe[individu.idgroupe]//3),individu.classe))
+            taille_groupe[individu.idgroupe]+=1
+            
+    groupe={}
+    for indi in l:
+        if indi.idgroupe not in groupe :
+            groupe[indi.idgroupe]=[]
+        groupe[indi.idgroupe].append(indi)
+    print(groupe)
+    for id_petit_groupe in groupe:
+        petit_groupe=groupe[id_petit_groupe]
+        for ind1 in petit_groupe:
+                for ind2 in petit_groupe:
+                    if ind1 != ind2:
+                        ind1.ajout_au_groupe(ind2)
+
+    return l
+
 if __name__ == '__main__':
     l = lirexcel2(0)
 
--- main.py
+++ main.py
@@ -3,34 +3,34 @@ from gurobipy import *
 import numpy as np
 from contraintes import *
 from objectif import *
-from lirexcel import lirexcel, lirexcel2
+from lirexcel import lirexcel, lirexcel2, reduction
 from affichage import affiche_texte, affiche_avion
 from tk_ffichage import new_aff
 
 
 N = 30
 P = 6
-scenario = 5
+scenario = 2
 
 
 
 if __name__ == '__main__':
     m=Model()
     ind=lirexcel2(scenario)
+    ind_reduit=reduction(scenario, ind)
     K=len(ind)
-    print(K)
     X=initialise(m,N,P,K)
     m.update()
-    barycentre(m,X,ind,N,P,K)
+    barycentre(m,X,ind_reduit,N,P,K)
     unicite_personne(m,X,N,P,K)
     unicite_siege(m,X,N,P,K)
-    chef_de_groupe(m, X, ind)
+    chef_de_groupe(m, X, ind_reduit)
     #symetrie(m,X,ind,N,P,K)
-    chaises_roulantes(m, X, ind)
-    civieres(m, X, ind)
-    nenfants(m,X,ind)
-    taille=lutte_des_classes(m,X,ind)
-    fct_objectif(m, X, ind)
+    chaises_roulantes(m, X, ind_reduit)
+    civieres(m, X, ind_reduit)
+    nenfants(m,X,ind_reduit)
+    taille=lutte_des_classes(m,X,ind_reduit)
+    fct_objectif(m, X, ind_reduit)
     m.update()
     m.optimize()
     affiche_texte(X.x,ind,m)
