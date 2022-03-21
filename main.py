b14cd10479082479c8180fb66a4ea27af98c70a8
Paul <paul.casteras@student-cs.fr>
2022-03-21 14:04:47 +0100

    save

--- main.py
+++ main.py
@@ -17,7 +17,8 @@ scenario = 2
 if __name__ == '__main__':
     m=Model()
     ind=lirexcel2(scenario)
-    ind_reduit=reduction(scenario, ind)
+    ind_reduit=ind
+    #reduction(scenario, ind)
     K=len(ind)
     X=initialise(m,N,P,K)
     m.update()
@@ -30,35 +31,11 @@ if __name__ == '__main__':
     civieres(m, X, ind_reduit)
     nenfants(m,X,ind_reduit)
     taille=lutte_des_classes(m,X,ind_reduit)
-    fct_objectif(m, X, ind_reduit, [0,0,0])
+    fct_objectif(m, X, ind_reduit, [0,2,2])
     m.update()
     m.optimize()
 
-
-    X_res=X.x
-
-
-    m2=Model()
-    Xbis=initialise(m2,N,P,K)
-    m2.update()
-    barycentre(m2,Xbis,ind_reduit,N,P,K)
-    unicite_personne(m2,Xbis,N,P,K)
-    unicite_siege(m2,Xbis,N,P,K)
-    chef_de_groupe(m2, Xbis, ind_reduit)
-    #symetrie(m,X,ind,N,P,K)
-    chaises_roulantes(m2, Xbis, ind_reduit)
-    civieres(m2, Xbis, ind_reduit)
-    nenfants(m2,Xbis,ind_reduit)
-    ligne(m2,X_res,Xbis,N,P,K)
-    taille2=lutte_des_classes(m2,Xbis,ind_reduit)
-    objectif_ligne(m2,X_res, Xbis, ind_reduit,N,P,K)
-
-    m2.update()
-    m2.optimize()
     affiche_texte(X.x,ind,m)
     affiche_avion(X.x,ind,m)
     new_aff(N, P, X.x, ind, m)
-    affiche_texte(Xbis.x,ind,m2)
-    affiche_avion(Xbis.x,ind,m2)
-    new_aff(N, P, Xbis.x, ind, m2)
     #print(taille2.x)
