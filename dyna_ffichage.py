a1a2a87d656e09e159e3ab284e77068d51614d52
Paul <paul.casteras@student-cs.fr>
2022-04-01 10:28:42 +0200

    correction

--- dyna_ffichage.py
+++ dyna_ffichage.py
@@ -130,8 +130,8 @@ def dyna_ffichage(N, P,K,tab,vrai_ind):
                 if i2==11 and secours==0:
                     secours+=1
                     nombre_groupe_secours[len(groupes[groupe_compteur])]=nombre_groupe_secours[len(groupes[groupe_compteur])]-1
-            canvas.itemconfig(places[i2][j2], fill='#00FF00')
-            X_nouveau[i2][j2][groupes[groupe_compteur][id].id]=1
+                canvas.itemconfig(places[i2][j2], fill='#00FF00')
+                X_nouveau[i2][j2][groupes[groupe_compteur][id].id]=1
         groupe_compteur+=1
         while groupe_compteur not in groupes and groupe_compteur<50000:
             groupe_compteur+=1
