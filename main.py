1949fc0213f7c386f12af3aaaedc62ac91e725bc
Pellerin Thibaut <thibaut.pellerin@student-cs.fr>
2022-04-01 17:44:50 +0200

    version finale

--- main.py
+++ main.py
@@ -8,11 +8,19 @@ scenario = 5
 
 
 if __name__ == '__main__':
-
-    X_post,ind,ind_reduit,best_score,_,_=meilleure_sol_statique(scenario,150, [0], [0])
+    # Renseigner le scénario, le temps maximal pour Gurobi, et les choix désirés pour le traitement des groupes.
+    X_post,ind,ind_reduit,best_score,a_best,b_best = meilleure_sol_statique(scenario, 60)
     (N,P,K)=X_post.shape
+
+    # Affichage de la solution statique
+    new_aff(N, P, X_post, ind)
+    print(f"Le placement des groupes est bon à {best_score}. On l'obtient pour {a_best, b_best}")
+
+    # Version dynamique
     nombre_choix=dyna_ffichage(N,P,K,X_post,ind,scenario)
-    x = [k for k in range(K)]
+
+    # Permet d'afficher les nombres de choix. Attention, ne fonctionne que pour les premières instances
+    #x = [k for k in range(K)]
     #y = [nombre_choix[k] for k in range(K)]
     # plt.plot(x,y)
     # plt.show()
